36fd63c41bea2be01b51782122e038a6bf2db98f
Nikita Koshelev <koshelev.nd@gmail.com>
2023-12-28 10:18:07 +0600

    refactor

--- recsys/infer.py
+++ recsys/infer.py
@@ -2,16 +2,14 @@ import pickle
 import sys
 import pandas as pd
 
-from recsys import models
+import models
 
 sys.modules['models'] = models
 items = pd.read_csv('data/items.csv')
-popularity_model = pickle.load(open('recsys/trained_models/popularity_based.pkl', 'rb'))
+popularity_model = pickle.load(open('trained_models/popularity_based.pkl', 'rb'))
 
 
-def get_movie():
+def get_movies():
 
-    preds = popularity_model.predict()
-    for pred in preds:
-        item = items[items['item_id'] == pred]
-        yield item.to_dict('records')[0]
+    most_popular = popularity_model.predict()
+    return most_popular
