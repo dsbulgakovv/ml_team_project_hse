c4a62d723cbcf0d7f5573974fedfa670e311abd7
Nikita Koshelev <koshelev.nd@gmail.com>
2023-12-28 10:24:12 +0600

    refactor

--- bot/app.py
+++ bot/app.py
@@ -9,15 +9,14 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemo
 from aiogram.utils.markdown import hbold
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
-from utils.db import check_user
-from handlers import user_info
-from handlers.user_info import User, age
+from utils.db import check_user, is_user_filled
+from handlers import user_info, recommendations
+from handlers.user_info import User
+from handlers.recommendations import Recommendation
 
 TOKEN = '6899841169:AAHfbiQQR_kOtiLEBqtzX-HbnPhizp5myME'  # TODO унести отсюда
 
 dp = Dispatcher()
-
-markup_text = "<b>Фильм:</b> {}\n\nГод выпуска: {}\n\nЖанры: {}\n\n\n{}"
 
 user_data = {}
 
@@ -30,7 +29,7 @@ class Form(StatesGroup):
 
 @dp.message(CommandStart())
 async def command_start_handler(message: types.Message, state: FSMContext) -> None:
-    """This handler receives messages with `/start` command"""
+    """Наполняем данные о пользователе если он новый, иначе сразу предлагаем фильм"""
 
     await message.answer(f"Привет, {hbold(message.from_user.full_name)}!")
     new_user = check_user(message.from_user.id)
@@ -39,44 +38,30 @@ async def command_start_handler(message: types.Message, state: FSMContext) -> No
         keyboard = ReplyKeyboardMarkup(keyboard=buttons, resize_keyboard=True)
         await message.answer("Поделитесь данными о себе", reply_markup=keyboard)
         await state.set_state(User.age)
-
-
-@dp.message(Form.movie_choice, F.text.casefold() == "посоветуй фильм")
-async def command_start_handler(message: types.Message) -> None:
-    """This handler receives request to show next movie"""
-
-    if message.from_user.id not in user_data:
-        user_data[message.from_user.id] = [1, 2, 3]
-    movie = next(user_data[message.from_user.id], None)
-    print(movie)
-    if movie:
-        response = markup_text.format(movie.get('title'), int(movie.get('release_year')),
-                                      movie.get('genres'), movie.get('description'))
-        await message.answer(response)
     else:
-        await message.answer('Для вас фильмы кончились :(', reply_markup=ReplyKeyboardRemove())
+        await state.set_state(Recommendation.popular)
+        keyboard = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="Поехали")]], resize_keyboard=True)
+        await message.answer("Посоветовать фильм?", reply_markup=keyboard)
 
 
-@dp.message(Form.movie_choice, F.text.casefold() == "остановитесь")
+@dp.message(F.text.casefold() == "поехали")
 async def command_start_handler(message: types.Message, state: FSMContext) -> None:
-    """This handler receives request to stop sending movies"""
+    """Проверяем заполнены ли у пользователя данные в БД и от этого решаем что покказать"""
 
-    await state.clear()
-    await message.answer('Ок, заканчиваю', reply_markup=ReplyKeyboardRemove())
+    filled = is_user_filled(message.from_user.id)
+    if filled:
+        await state.set_state(Recommendation.popular)
+    else:
+        await state.set_state(Recommendation.popular)
 
-
-#@dp.message()
-#async def echo_handler(message: types.Message) -> None:
-#    """ Forward received message back to the sender"""
-#    try:
-#        await message.send_copy(chat_id=message.chat.id)
-#    except TypeError:
-#        await message.answer("Nice try!")
+    buttons = [[KeyboardButton(text="Посоветуй фильм")]]
+    keyboard = ReplyKeyboardMarkup(keyboard=buttons, resize_keyboard=True)
+    await message.answer("Что делаем?", reply_markup=keyboard)
 
 
 async def main() -> None:
 
-    dp.include_routers(user_info.router)
+    dp.include_routers(user_info.router, recommendations.router)
     bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
 
     await dp.start_polling(bot)
