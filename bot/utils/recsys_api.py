7d368d203157740e776fe37fe27fe6934609aa60
Nikita Koshelev <koshelev.nd@gmail.com>
2024-01-09 19:16:11 +0600

    user-to-user recommendation to bot

--- bot/utils/recsys_api.py
+++ bot/utils/recsys_api.py
@@ -5,15 +5,20 @@ class RecsysAPI:
     def __init__(self):
         self.base_url = "http://recsys-service:8000"
 
-    async def get_popular(self):
+    async def get_personal(self, user_id):
         async with ClientSession() as session:
-            async with session.get(f"{self.base_url}/popular") as resp:
+            async with session.get(
+                f"{self.base_url}/popular", params={"id": user_id}
+            ) as resp:
                 response = await resp.json()
         return response
 
-    async def get_user_to_user(self):
+    async def get_user_to_user(self, content_type, genre):
         async with ClientSession() as session:
-            async with session.get(f"{self.base_url}/user_to_user") as resp:
+            async with session.get(
+                f"{self.base_url}/user_to_user",
+                params={"content_type": content_type, "genre": genre},
+            ) as resp:
                 response = await resp.json()
         return response
 
