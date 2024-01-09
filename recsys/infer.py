72dcfb8ae67cfafe4611cb4ed32d8fcb312336a7
Nikita Koshelev <koshelev.nd@gmail.com>
2024-01-09 19:22:05 +0600

    new dvc structure fix

--- recsys/infer.py
+++ recsys/infer.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 sys.modules["models"] = models
 items = pd.read_csv("data/items.csv")
-popularity_model = pickle.load(open("trained_models/popularity_based.pkl", "rb"))
+popularity_model = pickle.load(open("artifacts/popular/popularity_based.pkl", "rb"))
 
 
 def get_movies():
