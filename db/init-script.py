4653d28499614baa895b122dc7818bd90037f971
Nikita Koshelev <koshelev.nd@gmail.com>
2024-04-08 20:14:22 +0500

    init_db don't replace data

--- db/init-script.py
+++ db/init-script.py
@@ -10,13 +10,8 @@ log.setLevel(logging.DEBUG)
 @hydra.main(config_path="configs", config_name="cfg", version_base=None)
 def main(cfg):
 
-    from time import sleep
-
     import pandas as pd
     from sqlalchemy import create_engine
-
-    log.info("Sleeping for 10 seconds to allow postgresql init...")
-    sleep(10)
 
     log.info(f"Connection to the database: '{cfg.db.name}'...")
     db_address = (
@@ -28,32 +23,44 @@ def main(cfg):
     log.info("Connection established!")
 
     log.info(f"Loading file '{cfg.data.filenames.users}' to the database...")
-    df_users = pd.read_csv(cfg.data.dir_path + cfg.data.filenames.users)
-    df_users.to_sql(
-        name=cfg.db.tables.users, con=engine, index=False, if_exists="replace"
-    )
-    log.info("Successfully loaded!")
+    try:
+        df_users = pd.read_csv(cfg.data.dir_path + cfg.data.filenames.users)
+        df_users.to_sql(
+            name=cfg.db.tables.users, con=engine, index=False, if_exists="fail"
+        )
+        log.info("Successfully loaded!")
+    except ValueError:
+        log.info(f'File "{cfg.data.filenames.users} already loaded!')
 
-    log.info(f"Loading file '{cfg.data.filenames['items']}' to the database...")
-    df_items = pd.read_csv(cfg.data.dir_path + cfg.data.filenames["items"])
-    df_items.to_sql(
-        name=cfg.db.tables["items"], con=engine, index=False, if_exists="replace"
-    )
-    log.info("Successfully loaded!")
+    try:
+        log.info(f"Loading file '{cfg.data.filenames['items']}' to the database...")
+        df_items = pd.read_csv(cfg.data.dir_path + cfg.data.filenames["items"])
+        df_items.to_sql(
+            name=cfg.db.tables["items"], con=engine, index=False, if_exists="fail"
+        )
+        log.info("Successfully loaded!")
+    except ValueError:
+        log.info(f"File {cfg.data.filenames['items']} already loaded!")
 
     log.info(f"Loading file '{cfg.data.filenames.interactions}' to the database...")
-    df_interactions = pd.read_csv(cfg.data.dir_path + cfg.data.filenames.interactions)
-    df_interactions.to_sql(
-        name=cfg.db.tables.interactions, con=engine, index=False, if_exists="replace"
-    )
-    log.info("Successfully loaded!")
+    try:
+        df_interactions = pd.read_csv(cfg.data.dir_path + cfg.data.filenames.interactions)
+        df_interactions.to_sql(
+            name=cfg.db.tables.interactions, con=engine, index=False, if_exists="fail"
+        )
+        log.info("Successfully loaded!")
+    except ValueError:
+        log.info(f"File {cfg.data.filenames.interactions} already loaded!")
 
     log.info(f"Loading file '{cfg.data.filenames.links}' to the database...")
-    df_links = pd.read_csv(cfg.data.dir_path + cfg.data.filenames.links, sep="|")
-    df_links.to_sql(
-        name=cfg.db.tables.links, con=engine, index=False, if_exists="replace"
-    )
-    log.info("Successfully loaded!")
+    try:
+        df_links = pd.read_csv(cfg.data.dir_path + cfg.data.filenames.links, sep="|")
+        df_links.to_sql(
+            name=cfg.db.tables.links, con=engine, index=False, if_exists="fail"
+        )
+        log.info("Successfully loaded!")
+    except ValueError:
+        log.info(f"File {cfg.data.filenames.links} already loaded!")
 
 
 if __name__ == "__main__":
